4987b61e6ff287ffee8d54d822ec2429b44f6f3f
Doun Yun <106233376+yundoun@users.noreply.github.com>
2025-06-14 22:36:12 +0900

    Delete source directory

--- source/module-1/cdk/bin/cdk.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { WebApplicationStack } from "../lib/web-application-stack";
-
-const app = new cdk.App();
-new WebApplicationStack(app, "TodoList-Website");
